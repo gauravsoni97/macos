8668ba1bf7c4abf91d73c75d89a73970167ed300
gauravsoni97 <gauravsoni8414@gmail.com>
2022-08-29 20:01:52 +0530

    apple loader on start

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import FooterMenu from './Components/FooterMenu/FooterMenu'
 import "../src/index.css";
 
@@ -9,32 +9,44 @@ import {
 } from "react-router-dom";
 
 
-// pages 
-
-
+// pages imports
 import NewStart from './Components/AllDockPages/NewStart';
 import Finder from './Components/AllDockPages/Finder';
 import AppStore from './Components/AllDockPages/AppStore';
 import Login from './Components/Login/Login';
+import StartLoader from './Components/StartLoader/StartLoader';
 
 
 
 const App = () => {
 
+  const [loading, setLoading] = useState(false)
+
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 10000);
+  }, []);
+
+
 
   return (
     <>
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NewStart />} />
-          <Route path="finder" element={<Finder />} />
-          <Route path="appstore" element={<AppStore />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      {loading ? <StartLoader /> :
 
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<NewStart />} />
+            <Route path="finder" element={<Finder />} />
+            <Route path="appstore" element={<AppStore />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
 
+      }
 
 
     </>
---
+++ src/Components/StartLoader/StartLoader.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./StartLoader.css"
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+
+import loader from "../../Imgs/appleLogo/appleloader.png"
+
+
+
+const StartLoader = () => {
+
+
+    const [progress, setProgress] = React.useState(0);
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((oldProgress) => {
+
+                const diff = Math.random() * 10;
+                return Math.min(oldProgress + diff, 100);
+            });
+        }, 500);
+
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return (
+        <>
+            <div className="AppleLoader">
+                <div className="mainBox">
+                    <div className="Applelogo">
+                        <img className='ApplelogoImg' src={loader} alt="" />
+                    </div>
+                    <br />
+                    <div className="loading_animation">
+                        <Box sx={{ width: '100%' }} >
+                            <LinearProgress className='progressbar' variant="determinate" value={progress} />
+                        </Box>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+}
+
+export default StartLoader
