cda950d87fdc9bbe18b21c4b83850b70ad98e127
gauravsoni97 <gauravsoni8414@gmail.com>
2022-09-05 15:11:30 +0530

    before applying usecontext and all set

--- src/Components/AllDockPages/Finder.jsx
+++ src/Components/AllDockPages/Finder.jsx
@@ -2,22 +2,13 @@ import React from 'react'
 import Main from '../Main/Main'
 import './DockPages.css'
 import folder from "../../Imgs/folderimg/folder.png"
-
 import "../../Components/PlaygroundArea/NewFolder/OpenWindow.css"
-import OpenWindowLeft from '../PlaygroundArea/NewFolder/OpenWindowLeft'
-import OpenWindowRight from '../PlaygroundArea/NewFolder/OpenWindowRight'
-import NewFolderOpenWindow from '../PlaygroundArea/NewFolder/NewFolderOpenWindow'
 
 
 const Finder = () => {
     return (
         <>
             <Main />
-
-
-
-
-            <NewFolderOpenWindow />
 
         </>
     )
