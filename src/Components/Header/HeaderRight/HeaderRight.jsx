c55bced3181d63128e120f371bbf8b6b6afe4ab4
gauravsoni97 <gauravsoni8414@gmail.com>
2022-09-06 12:17:52 +0530

    spotlight added search bar

--- src/Components/Header/HeaderRight/HeaderRight.jsx
+++ src/Components/Header/HeaderRight/HeaderRight.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./HeaderRight.css"
 import Battery from './MenuItemsHeaderRight/Battery'
 import DateandTime from './MenuItemsHeaderRight/DateandTime'
@@ -11,6 +11,8 @@ import Wifi from './MenuItemsHeaderRight/Wifi'
 
 
 const HeaderRight = () => {
+
+  
   return (
     <div className='header_right' >
       <ul className='header_right_menu'>
@@ -22,7 +24,10 @@ const HeaderRight = () => {
 
         {/* ------------------------------------  Search - Header right  */}
 
+
         <SearchTab />
+
+    
         {/* ------------------------------------  Double Toggle - Header right  */}
 
         <DoubleToggle />
--- src/Components/Header/HeaderRight/MenuItemsHeaderRight/SearchTab.jsx
+++ src/Components/Header/HeaderRight/MenuItemsHeaderRight/SearchTab.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import search from "../../../../Imgs/HeaderRight/search.png"
 
 
 const SearchTab = () => {
+
+  const [spotlight, setspotlight] = useState("");
+  const [spotlightSearch, setSpotlightSearch] = useState(false)
+
+
+  const showSearchBar = () => {
+    return setSpotlightSearch(true)
+  }
+  const closeSearchbar = () => {
+    return setSpotlightSearch(false)
+  }
+
+
+
   return (
     <>
-     <li className='header_right_menu_item' ><img className='header_right_itemimg'  src={search} alt="" /></li>
+      <li className='header_right_menu_item' onDoubleClick={closeSearchbar} onClick={showSearchBar} ><img className='header_right_itemimg' src={search} alt="" /></li>
+
+      {spotlightSearch ?
+        <div className="quicksearchbar-div">
+          <i class="uil uil-search spotlight_search_icon"></i>
+          <input className='quicksearchbar' type="text" name='text' placeholder='Spotlight Search' value={spotlight} onChange={(e) => setspotlight(e.target.value)} />
+        </div>
+        : ""}
 
     </>
   )
