91518eb6f79850b527d7100e0cef417445b4b0a3
gauravsoni97 <gauravsoni8414@gmail.com>
2022-09-08 09:19:12 +0530

    time set

--- src/Components/Header/HeaderRight/MenuItemsHeaderRight/DateandTime.jsx
+++ src/Components/Header/HeaderRight/MenuItemsHeaderRight/DateandTime.jsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react'
 
 const DateandTime = () => {
   let currentTime = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-  const [time, settime] = useState(currentTime)
 
+  const [currentDateTime, setCurrentDateTime] = useState(() => currentTime);
   useEffect(() => {
-    setInterval(() => {
-      settime(time)
+    const secondsTimer = setInterval(() => {
+      setCurrentDateTime(new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
     }, 1000);
-
-  },[time])
+    return () => clearInterval(secondsTimer);
+  }, [setCurrentDateTime]);
 
   return (
     <>
       <li className=' header_right_menu_item-text'>{new Date().toLocaleDateString('en-us', { weekday: "short", day: "numeric", month: "short" })}</li>
       <li className=' header_right_menu_item-text'>
-        {time}
+        {currentDateTime}
       </li>
 
     </>
