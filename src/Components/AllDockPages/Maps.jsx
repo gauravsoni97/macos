e7f6002c491084e64a6eab8b1d3eb8e7a6ad7828
gauravsoni97 <gauravsoni8414@gmail.com>
2022-09-08 10:57:47 +0530

    maps set

--- src/Components/AllDockPages/Maps.jsx
+++ src/Components/AllDockPages/Maps.jsx
@@ -65,7 +65,7 @@ const Maps = () => {
                 <p className="menuItemHeading">My Guides</p>
                 <ul className="menuItems">
                   <span className="item-box">
-                  <i class="uil uil-plus-circle" style={{color:"gray"}}></i>
+                    <i class="uil uil-plus-circle" style={{ color: "gray" }}></i>
                     <p className=' item_name_map'>Add Guide</p>
 
                   </span>
@@ -78,10 +78,25 @@ const Maps = () => {
 
           </div>
           <div className="maps-right">
-            <div className="maps-header-right">
-
+            <div className="maps-header-right d_flex">
+              <div className="left-sideof-mapsheader d_flex">
+                <i class="uil uil-window"></i>
+                <p className='location_text'>Ellenabad - Sirsa</p>
+              </div>
+              <div className="right-sideof-mapsheader">
+              <i class="uil uil-telegram-alt"></i>
+              <i class="uil uil-map"></i>
+              <i class="uil uil-corner-up-right"></i>
+              <i class="uil uil-plus"></i>
+              <i class="uil uil-upload"></i>
+              <i class="uil uil-user-circle"></i>
+              </div>
             </div>
+
+
+
             <div className="Actuall_map">
+              <iframe className='mapgraph' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27793.300518137017!2d74.64880559328567!3d29.453262973615537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3916bcb50b4d5977%3A0x2070584ab75be3e7!2sEllenabad%2C%20Haryana%20125102%2C%20India!5e0!3m2!1sen!2sus!4v1662612764855!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
             </div>
           </div>
