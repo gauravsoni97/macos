ad17c5ea191fcbd99a7ef97715f640433422909c
gauravsoni97 <gauravsoni8414@gmail.com>
2022-09-08 09:28:31 +0530

    wifi names

--- src/MapData/WifiNames.js
+++ src/MapData/WifiNames.js
@@ -1,32 +1,43 @@
 export const WifiNamesData = [
     {
         id:1,
-        wifi:"Wifi Name here",
+        wifi:"Free ka Maal",
 
     },
     {
         id:2,
-        wifi:"Wifi Name here",
+        wifi:"Baba Bangali",
 
     },
     {
         id:3,
-        wifi:"Wifi Name here",
+        wifi:"Kala Kauwa",
 
     },
     {
         id:4,
-        wifi:"Wifi Name here",
+        wifi:"10Rs Me 10Minutes",
 
     },
     {
         id:5,
-        wifi:"Wifi Name here",
+        wifi:"Muft Wifi Suvidha",
 
     },
     {
         id:6,
-        wifi:"Wifi Name here",
+        wifi:"Majboot Wifi",
+
+    },
+ 
+    {
+        id:7,
+        wifi:"Kalu Don",
+
+    },
+    {
+        id:8,
+        wifi:"Janhit Mein Jaari",
 
     },
 ]
